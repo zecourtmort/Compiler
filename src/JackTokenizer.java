49569fcb24ce67b71e9140700633bcd1a9ac0719
Alex Manoilo <amanoilo@mymail.mines.edu>
2016-04-22 10:30:27 -0600

    Remember, suicide prevention week was pretty recent

--- src/JackTokenizer.java
+++ src/JackTokenizer.java
@@ -2,12 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class JackTokenizer{
+	
+	private Scanner scan;
+    private String cmd;
+    private char[] symboltable = {'{', '}', '(' ,')', '[' , ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~'};
 	public JackTokenizer(String filename){
-		
+		File infile = new File(filename);
+		try {
+	           scan = new Scanner(infile);
+	           String lines = "";
+	           String line = "";
+	           
+	           while(scan.hasNext()) {
+	               line = removeComments(scan.nextLine()).trim();
+	               
+	               if (line.length() > 0) {
+	                   lines += line + "\n";
+	               }	
+	           }
+	           scan = new Scanner(lines.trim());
+	        }
+	        
+	        catch(FileNotFoundException e){
+	            System.out.println(infile + " is not a valid file.");
+	        }
+	        /*cmds.add("add");
+	        cmds.add("sub");
+	        cmds.add("neg");
+	        cmds.add("eq");
+	        cmds.add("gt");
+	        cmds.add("lt");
+	        cmds.add("and");
+	        cmds.add("or");
+	        cmds.add("not");*/
 	}
+	
+	public void advance(){
+        cmd = scan.nextLine();
+       String[] splitcmd = cmd.split(" ");
+       /*arg1 = "";
+       arg2 = -1;*/
+       if(splitcmd.length > 3){
+           throw new IllegalArgumentException("Too many arguments");
+       }
+       
 	public boolean hasMoreTokens(){
-		return false;
+		return scan.hasNext();
 	}
+	
 	public tokenType tokenType(){
 		return null;
 	}
@@ -26,4 +68,16 @@ public class JackTokenizer{
 	public String stringVal(){
 		return null;
 	}
+	public static String removeComments(String s){
+        int pos = s.indexOf("//");
+        if (pos!=-1) s = s.substring(0, pos);
+        return s;
+    }
+    
+    public static String extension(String path) {
+        int pos = path.indexOf(".");
+        String fileExtension = "";
+        if (pos!=-1) fileExtension = path.substring(pos, path.lastIndexOf(path));
+        return fileExtension;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -3,7 +3,92 @@ import java.io.IOException;
 import java.util.*;
 import java.io.*;
 public class main{
+	
 	public static void main(String args[]){
-		
+		// TODO code application logic here
+        if (args.length != 1) {
+            System.out.println("Please enter: java VMtranslator FILENAME|DIRECTORY");
+        }
+        else{
+            File infile = new File(args[0]);
+            String outfiledirectory = "";
+            File outfile;
+            CodeWriter writer;
+            ArrayList<File> vm = new ArrayList<File>();
+            
+            if (infile.isFile()) {
+                String path = infile.getAbsolutePath();
+                
+                if(!Parser.extension(path).equals(".vm")) {
+                    throw new IllegalArgumentException(".vm file required.");
+                }
+                
+                vm.add(infile);
+                outfiledirectory = path.substring(0, path.lastIndexOf(".")) + ".asm";    
+            }
+            
+            else if (infile.isDirectory()) {
+                vm = getFiles(infile);
+                
+                if (vm.isEmpty()){
+                    throw new IllegalArgumentException("No vm files here");
+                }
+                
+                outfiledirectory = infile.getAbsolutePath() + "/" + infile.getName() + ".asm";
+            }
+                
+            outfile = new File(outfiledirectory);
+            writer = new CodeWriter(outfile);
+            
+            //writer.writeInit();
+            for (File f: vm) {
+                Parser parser = new Parser(f);
+                int type;
+                
+                while (parser.hasMoreCommands()) {
+                    parser.advance();
+                    type = parser.commandType();
+                    
+                    if (type == Parser.ARITHMETIC) {
+                        writer.writeArithmetic(parser.arg1());
+                    }
+                    else if (type == Parser.POP || type == Parser.PUSH){
+                        writer.writePushPop(type, parser.arg1(), parser.arg2());
+                    }
+                    else if (type == Parser.LABEL){
+                        writer.writeLabel(parser.arg1());
+                    }
+                    else if (type == Parser.GOTO) {
+                        writer.writeGoto(parser.arg1());
+                    }
+                    else if (type == Parser.IF) {
+                        writer.writeIf(parser.arg1());
+                    }
+                    else if (type == Parser.RETURN) {
+                        writer.writeReturn();
+                    }
+                    else if (type == Parser.FUNCTION) {
+                        writer.writeFunction(parser.arg1(),parser.arg2());
+                    } 
+                    else if (type == Parser.CALL) {
+                        writer.writeCall(parser.arg1(),parser.arg2());
+                    }
+                }
+            }
+            writer.close();
+            
+            System.out.println("New file here: " + outfiledirectory);
 	}
+        
+        class VMTranslator {
+            public static ArrayList<File> getFiles(File dir) {
+                File[] fs = dir.listFiles();
+                ArrayList<File> files = new ArrayList<File>();
+                for (File f:fs){
+                    if (f.getName().endsWith(".vm")) files.add(f);
+                }
+                return files;
+            }
+        }
+}
 }
