5a3d6374e26775f2c863e72ec986777c08829d27
Courtney Mort <cmort@mines.edu>
2016-05-09 22:13:59 -0600

    Probably working Tokenizer

--- src/JackTokenizer.java
+++ src/JackTokenizer.java
@@ -25,12 +25,32 @@ public class JackTokenizer{
 	           scan = new Scanner(infile);
 	          String lines = "";
 	           String line = "";
-	           
+	           boolean multiLineComment = false;
 	           while(scan.hasNext()) {
-	               line = removeComments(scan.nextLine()).trim();
+	               line = removeComments(scan.nextLine()).trim();	
+	               boolean end = false;
+	               int posstar = line.indexOf("/*");
+	               	if(line.contains("*/")){
+	               		end = true;
+	               	}
+	               if(posstar !=-1){
+	               	line=line.substring(0, posstar);
+	               	while(!end){
+	               		String hol;
+	               		if(scan.hasNext()){
+	               			hol = scan.nextLine();
+		               		if(hol.contains("*/")){
+		               			end = true;
+		               		}
+	               		}
+	               		else{
+	               			end = true;
+	               		}
+	               	}
+	               }
 	               if (line.length() > 0) {
 	                   lines += line + "\n";
-	               }	
+	               }
 	           }
 	           scan = new Scanner(lines.trim());
 	        }
@@ -62,12 +82,20 @@ public class JackTokenizer{
         		}
         }
         else if(tkn.contains(".")){
+        		System.out.println("here");
         		tkns = tkn.replace("."," ");
+        		System.out.println(tkns);
         		tknsplit = tkns.split(" ");
+        		System.out.println(tknsplit[0]);
         		token identifier = new token(tokenTypes.IDENTIFIER, tknsplit[0]);
+        		System.out.println(identifier.toString());
+        		System.out.println(Tokens.size());
         		Tokens.add(identifier);
+        		System.out.println(Tokens.size());
         		Tokens.add(new token(tokenTypes.SYMBOL, "."));
+        		System.out.println(Tokens.size());
         		parseParens(tknsplit[1]);
+
         }
         else if(tkn.contains("[")){
         	int arguments=0;
@@ -116,7 +144,19 @@ public class JackTokenizer{
         		}
         	}
         	else{
-        		if(tkn.contains(";")){
+        		if(tkn.contains(")")){
+        			tkns = tkn.replace(")"," ");
+        			tknsplit = tkns.split(" ");
+        			Tokens.add(new token(tokenTypes.IDENTIFIER,tknsplit[0]));
+        			Tokens.add(new token(tokenTypes.SYMBOL, ")"));
+        			if(tknsplit.length>1){
+        				if(tknsplit[1].contains(";")){
+                			token semi = new token(tokenTypes.SYMBOL, ";");
+                			Tokens.add(semi);
+        				}
+        			}
+        		}
+        		else if(tkn.contains(";")){
         			tkns = tkn.replace(";","");
         			token identifier = new token(tokenTypes.IDENTIFIER, tkns);
         			Tokens.add(identifier);
@@ -165,9 +205,10 @@ public class JackTokenizer{
 	public String stringVal(){
 		return null;
 	}
-	public static String removeComments(String s){
+	public String removeComments(String s){
         int pos = s.indexOf("//");
         if (pos!=-1) s = s.substring(0, pos);
+
         return s;
     }
     public static String extension(String path) {
@@ -178,12 +219,25 @@ public class JackTokenizer{
     }
     public boolean parseParens(String tkn){
     	if(tkn.charAt(0)=='('){
+    		boolean end = false;
+    		System.out.println("ifififififif");
     		Tokens.add(new token(tokenTypes.SYMBOL, "("));
     		String holdst = tkn.replace("(", "");
-    		Tokens.add(new token(tokenTypes.IDENTIFIER, holdst));
-    		boolean end = false;
+    		if(holdst.contains(")")){
+    			holdst = holdst.replace(")", "");
+    			Tokens.add(new token(tokenTypes.IDENTIFIER, holdst));
+    			Tokens.add(new token(tokenTypes.SYMBOL,")"));
+    			if(tkn.contains(";")){
+    				Tokens.add(new token(tokenTypes.SYMBOL,";"));
+    			}
+    			end = true;
+    		}
+    		else{
+    			Tokens.add(new token(tokenTypes.IDENTIFIER, holdst));
+    		}
     		while (!end){
     			String hol = scan.next();
+    			System.out.println(hol);
     			if(hol.contains(")")){
     				end = true;
     				hol = hol.replace(")","");
@@ -224,6 +278,9 @@ public class JackTokenizer{
     	if(tkn.contains("()")){
     		token paren2 = new token(tokenTypes.SYMBOL, ")");
     		Tokens.add(paren2);
+    		if(tkn.contains(";")){
+    			Tokens.add(new token(tokenTypes.SYMBOL,";"));
+    		}
     	}
     	else if(tknsplit[1].contains("\"")){
     		String hol;
@@ -281,7 +338,14 @@ public class JackTokenizer{
             	}
             }
             else{
+        		if(tknsplit[1].contains(",")){
+        			String strcon=tknsplit[1].replace(",","");
+        			Tokens.add(new token(tokenTypes.IDENTIFIER,strcon));
+        			Tokens.add(new token(tokenTypes.SYMBOL,","));
+        		}
+        		else{
             	Tokens.add(new token(tokenTypes.IDENTIFIER, tknsplit[1]));
+        		}
             }
     	return true;
     	}
